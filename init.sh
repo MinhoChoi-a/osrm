51edd861d466ca9fe6edc5f989cbb5bf7f1d43fe
minhochoi-a <minhocalgary@gmail.com>
2023-04-20 21:32:40 -0600

    updated init script

--- init.sh
+++ init.sh
@@ -5,6 +5,9 @@
 #sudo chmod +x ./init.sh
 #sudo mv ./init.sh /home/ubuntu/init.sh
 
+#!/bin/bash
+
+#sudo apt-get update
 sudo wget http://nginx.org/keys/nginx_signing.key
 sudo apt-key add nginx_signing.key
 
@@ -15,35 +18,36 @@ sudo echo "deb-src http://nginx.org/packages/ubuntu "$(. /etc/os-release && echo
 sudo apt-get update
 sudo apt-get install nginx
 sudo systemctl start nginx.service
-
+#
 cd /etc/nginx/conf.d
 sudo mv ./default.conf ./default.conf.bak
 sudo touch ./server1.conf
 sudo echo "server { root /home/ubuntu/public_html; location /osrm/route/default { proxy_pass http://localhost:5000/route/v1/driving; } location /osrm/route/avoidtoll { proxy_pass http://localhost:5001/route/v1/driving; } }" > ./server1.conf
 sudo nginx -s reload
 
-cd ~
-sudo apt-get install ca-certificates curl gnupg
-sudo mkdir -m 0755 -p /etc/apt/keyrings
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
-
-echo "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-
-sudo apt-get update
-sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
-
-sudo mkdir /home/ubuntu/osrm_docker
-sudo mkdir /home/ubuntu/osrm_docker/default
-sudo mkdir /home/ubuntu/osrm_docker/avoidtoll
-
-sudo cp /home/ubuntu/osrm/default/car.lua /home/ubuntu/osrm_docker/default/car.lua
-sudo cp /home/ubuntu/osrm/avoidtoll/car.lua /home/ubuntu/osrm_docker/avoidtoll/car.lua
-
-sudo cp /home/ubuntu/osrm/default/Dockerfile /home/ubuntu/osrm_docker/default/Dockerfile
-sudo cp /home/ubuntu/osrm/avoidtoll/Dockerfile /home/ubuntu/osrm_docker/avoidtoll/Dockerfile
-
-cd /home/ubuntu/osrm_docker
-wget http://download.geofabrik.de/north-america/canada/ontario-latest.osm.pbf
+#
+#cd ~
+#sudo apt-get install ca-certificates curl gnupg
+#sudo mkdir -m 0755 -p /etc/apt/keyrings
+#curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+#
+#echo "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+#
+#sudo apt-get update
+#sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+#
+#sudo mkdir /home/ubuntu/osrm_docker
+#sudo mkdir /home/ubuntu/osrm_docker/default
+#sudo mkdir /home/ubuntu/osrm_docker/avoidtoll
+#
+#sudo cp /home/ubuntu/osrm/default/car.lua /home/ubuntu/osrm_docker/default/car.lua
+#sudo cp /home/ubuntu/osrm/avoidtoll/car.lua /home/ubuntu/osrm_docker/avoidtoll/car.lua
+#
+#sudo cp /home/ubuntu/osrm/default/Dockerfile /home/ubuntu/osrm_docker/default/Dockerfile
+#sudo cp /home/ubuntu/osrm/avoidtoll/Dockerfile /home/ubuntu/osrm_docker/avoidtoll/Dockerfile
+#
+#cd /home/ubuntu/osrm_docker
+#wget http://download.geofabrik.de/north-america/canada/ontario-latest.osm.pbf
 
 #cd /home/ubuntu/osrm_docker/default
 #sudo docker build -t osrm-default .
